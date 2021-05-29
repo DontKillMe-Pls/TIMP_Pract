017e84cc6589a8f9423d44bfa7c958ec5ee8387f
DontKillMe-Pls <65929904+DontKillMe-Pls@users.noreply.github.com>
2021-05-28 22:14:55 -0400

    Add files via upload

---
+++ modAlphaCipher.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <cctype>
+#include <vector>
+#include <string>
+#include <map>
+ 
+class modAlphaCipher
+{
+private:
+	
+	std::map <char,int> alphaNum;
+	std::vector <int> key;
+	std::vector <int> convert(const std::wstring& s);
+	std::wstring convert(const std::vector<int>& v);
+public:
+	bool isValid(const std::wstring& s);
+	modAlphaCipher(const std::wstring& skey);
+	std::wstring encrypt(const std::wstring& open_text);
+	std::wstring decrypt(const std::wstring& cipher_text);
+	modAlphaCipher() = delete;
+	std::wstring numAlpha = L"ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+	std::wstring list_of_alph_for_valid = L"abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфчцчшщъыьэюя";
+	std::wstring getValidText(const std::wstring& not_valid_open_text);
+	
+};
+
+
